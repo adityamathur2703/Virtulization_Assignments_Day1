10aa8f6a4c9ac6bb686675a4bab9701ec4f66509
adityamathur2703 <mathuraditya.270302@gmail.com>
2024-10-23 18:38:52 +0530

    First Commit

--- Engine_temp.cpp
+++ Engine_temp.cpp
@@ -1,16 +1,13 @@
 #include "Engine_temp.h"
 
-// Constructor definition
 EngineTemperatureControl::EngineTemperatureControl(float activationTemp) 
     : engineTemperature(0), fanActivationTemp(activationTemp), fanStatus(false) {}
 
-// Method to update the engine temperature and control the fan
 void EngineTemperatureControl::updateTemperature(float currentTemp) {
     engineTemperature = currentTemp;
     controlFan();  // Check and update fan status based on temperature
 }
 
-// Method to control the fan based on the current engine temperature
 void EngineTemperatureControl::controlFan() {
     if (engineTemperature >= fanActivationTemp) {
         fanStatus = true;  // Turn on the fan
@@ -19,7 +16,6 @@ void EngineTemperatureControl::controlFan() {
     }
 }
 
-// Method to print the current status of the system
 void EngineTemperatureControl::printStatus() const {
     std::cout << "Engine Temperature: " << engineTemperature << " °C\n";
     std::cout << "Fan Status: " << (fanStatus ? "ON" : "OFF") << "\n";
